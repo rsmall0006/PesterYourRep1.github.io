549fafaf1a9fe1ae6dffb3bc10ff3e5f95711dba
Ross Small <Merlin@rosss-mbp.nvstu.edu>
2017-04-26 15:49:01 -0400

    added WebMvcConfigurer to the Main Class

--- src/main/java/com/pester/PesterYourRepApplication.java
+++ src/main/java/com/pester/PesterYourRepApplication.java
@@ -2,6 +2,10 @@ package com.pester;
 import com.pester.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by octavio on 4/25/17.
@@ -10,7 +14,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PesterYourRepApplication {
 
     public static void main(String[] args) {SpringApplication.run(PesterYourRepApplication.class, args); }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 
 }
